79deb7508c6289e2c50b5136cb230b0b7d9d7add
Danny van der Jagt <dannyvanderjagt@gmail.com>
2014-05-26 10:48:23 +0200

    Added some pages and did some UI work.
    Added some pages and did some UI work.

---
+++ Word-search-game/Word-search-game.Windows/Pages/GamePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Word_search_game.Classes;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Word_search_game
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GamePage : Page
+    {
+        public GamePage()
+        {
+            this.InitializeComponent();
+            Boggle boggle = new Boggle("easy", 1, tileGrid, wordList); // Difficulty, Level, grid for the tiles Element, grid for the words element.
+
+        }
+    }
+}
